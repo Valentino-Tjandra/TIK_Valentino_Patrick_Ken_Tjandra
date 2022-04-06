65b8fe50035365128c358b25aeab7535bc2f6c66
Valentino-Tjandra <103082819+Valentino-Tjandra@users.noreply.github.com>
2022-04-06 09:15:25 +0800

    Create variabel.cpp

---
+++ variabel.cpp
@@ -0,0 +1,9 @@
+//header
+#include<iostream> // input output iosstream
+using namespace std;
+
+int main()  { //begin of function
+
+cout<<"Valentino Patrick Ken Tjandra";
+     
+}// end of function
