47a2f66cde6d39262b0de0e2eb5ab781d6e8eeaa
Valentino-Tjandra <103082819+Valentino-Tjandra@users.noreply.github.com>
2022-04-13 09:33:36 +0800

    Create 20220413_if.cpp

---
+++ 20220413_if.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    /*
+    jika nilai >= 90 lulus memuaskan
+    jika nilai >= 90 lulus memuaskan
+    jika nilai >= 75 cukup
+    tidak boleh < 90
+    < 75 tidak lulus
+    
+     < 75 tidak lulus
+     
+   */
+    int nilai;
+    cout <<"Masukkan Nilai = "; cin>>nilai;
+        if (nilai > 90) {
+        cout <<"lulus sangat memuaskan" ;
+        }
+    else if (nilai >= 80 ) {
+        cout <<"lulus memuaskan";
+    }
+    else if (nilai >= 75  ) {
